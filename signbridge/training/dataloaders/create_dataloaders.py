ab2d34e731772b31753d50854c303f1a29b53790
vamsi54-bit <vamsiskn@gmail.com>
2026-09-05 17:19:50 +0530

    writed dataloaders

---
+++ signbridge/training/dataloaders/create_dataloaders.py
@@ -0,0 +1,64 @@
+from torch.utils.data import DataLoader
+
+from training.datasets.sign_dataset import (
+    SignDataset
+)
+
+from training.datasets.split_dataset import (
+    split_dataset
+)
+
+
+BATCH_SIZE = 32
+
+
+def create_dataloaders(
+    batch_size=BATCH_SIZE
+):
+
+    dataset = SignDataset()
+
+    train_dataset, val_dataset, test_dataset = (
+        split_dataset(dataset)
+    )
+
+    train_loader = DataLoader(
+
+        train_dataset,
+
+        batch_size=batch_size,
+
+        shuffle=True,
+
+        num_workers=0
+    )
+
+    val_loader = DataLoader(
+
+        val_dataset,
+
+        batch_size=batch_size,
+
+        shuffle=False,
+
+        num_workers=0
+    )
+
+    test_loader = DataLoader(
+
+        test_dataset,
+
+        batch_size=batch_size,
+
+        shuffle=False,
+
+        num_workers=0
+    )
+
+    return (
+        train_loader,
+        val_loader,
+        test_loader
+    )
+
+
